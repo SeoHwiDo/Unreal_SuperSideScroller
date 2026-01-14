d48fc46aa7abb2d5b689e0a6fde500a827402434
shd <masterseo01@naver.com>
2026-01-14 18:21:32 +0900

    [add] 플레이어 발사체 구현을 위한 코드 작성

---
+++ Source/SuperSideScroller/PlayerProjectile.h
@@ -0,0 +1,32 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "PlayerProjectile.generated.h"
+
+UCLASS()
+class SUPERSIDESCROLLER_API APlayerProjectile : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	APlayerProjectile();
+	UPROPERTY(VisibleDefaultsOnly, Category = Projectile)
+	class USphereComponent* CollisionComp;
+
+	UFUNCTION()
+	void OnHit(UPrimitiveComponent* HitComp, AActor* OhterActor, UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit);
+private:
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Movement, meta = (AllowPrivateAccess = "true"))
+	class UprojectileMovementComponent* ProjectileMovement;
+	
+	UPROPERTY(VisibleDefaultsOnly, Category = Projectile)
+	class UStaticMeshComponent* MeshComp;
+protected:
+	
+
+
+};

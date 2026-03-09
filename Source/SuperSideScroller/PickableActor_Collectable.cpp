aac145d9e3ed59a98bcc0b1401a18a4955016f3b
shd <masterseo01@naver.com>
2026-03-09 22:02:34 +0900

    [add] Collectable 코인 구현

---
+++ Source/SuperSideScroller/PickableActor_Collectable.cpp
@@ -0,0 +1,12 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "PickableActor_Collectable.h"
+
+void APickableActor_Collectable::PlayerPickedUp(class ASuperSideScroller_Player* Player) {
+	Super::PlayerPickedUp(Player);
+
+}
+void APickableActor_Collectable::BeginPlay() {
+	Super::BeginPlay();
+}
